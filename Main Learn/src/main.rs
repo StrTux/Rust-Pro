b3d3dd9b81322efd1904c9b1bbcd68b8757f71cc
Ashish Tiwari <abhitiwari9857@gmail.com>
2025-07-30 20:23:36 +0530

    Hashmap: Function Changes

--- Main Learn/src/main.rs
+++ Main Learn/src/main.rs
@@ -1,22 +1,3 @@
-use std::collections::HashMap;
-
-
 fn main() {
-    let mut names: HashMap<&str , i32> = HashMap::new();
-
-    names.insert("ashish", 420);
-    names.insert("nikki", 360);
-    names.insert("soni", 350);
-    
-    println!("this all the names inside this {:?}", names);
-
-    match names.get("ashish")  {
-        Some(name)=> println!("this is the names of {:?}", name),
-        None => println!("there is no  data name nikki")
-    }
-
-    for (subject, name) in &names {
-        println!("for {} you  got names {}", subject, name);
-    }
-    println!("for the  finding is avaible or not in the names : {:?}", names.contains_key("Sushila"))
+    println!("this function are been  hashmap")
 }
